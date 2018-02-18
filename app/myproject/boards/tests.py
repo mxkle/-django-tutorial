25b03e26fdf26698c2ef4c7287732b19f00904d7
mxkle <mxkle@virtual.lan>
2018-02-18 17:27:24 +0100

    modified test file, added views test

--- app/myproject/boards/tests.py
+++ app/myproject/boards/tests.py
@@ -1,12 +1,14 @@
 from django.core.urlresolvers import reverse
+from django.urls import resolve
 from django.test import TestCase
+from .views import boards
 
-class HomeTests(TestCase):
-    def test_index_view_status_code(self):
-        url = reverse('index')
+class BoardsTests(TestCase):
+    def test_boards_view_status_code(self):
+        url = reverse('boards')
         response = self.client.get(url)
         self.assertEquals(respone.status_code, 200)
 
     def test_index_url_resolves_home_view(self):
-        view = resolve('/')
-        self.assertEquals(view.func, index)
+        view = resolve('/boards')
+        self.assertEquals(view.func, boards)
