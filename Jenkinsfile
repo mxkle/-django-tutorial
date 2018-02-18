0f4acb6845dd170ff692d5d722c25b40d39ba44a
mxkle <mxkle@virtual.lan>
2018-02-18 17:16:09 +0100

    added test

--- Jenkinsfile
+++ Jenkinsfile
@@ -4,12 +4,17 @@ pipeline {
       image 'python:3'
       args '-v /app:/app -p 5000:5000'
     }
-    
+
   }
   stages {
     stage('build') {
       steps {
         sh 'pip install --no-cache-dir -r app/requirements.txt'
+      }
+    }
+    stage('test') {
+      steps {
+        sh 'python3 app/myproject/manage.py test'
       }
     }
     stage('run') {
--- app/myproject/boards/tests.py
+++ app/myproject/boards/tests.py
@@ -1,3 +1,12 @@
+from django.core.urlresolvers import reverse
 from django.test import TestCase
 
-# Create your tests here.
+class HomeTests(TestCase):
+    def test_index_view_status_code(self):
+        url = reverse('index')
+        response = self.client.get(url)
+        self.assertEquals(respone.status_code, 200)
+
+    def test_index_url_resolves_home_view(self):
+        view = resolve('/')
+        self.assertEquals(view.func, index)
